148ed8fe6ca8a9df377f7f9d67f088781b2d6a52
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-04 20:32:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  array = []
+  if name_hash == {}
+    return nil
+  end
+  
   name_hash.collect do | k, v |
     if v < 
   end
